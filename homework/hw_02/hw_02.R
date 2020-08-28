e4640b8dcb2de0087a9b224dfa6b0802b1e6a2c9
Frank May <fmay@vols.utk.edu>
2020-08-28 17:10:19 -0400

    Homework 2 Commit

---
+++ homework/hw_02/hw_02.R
@@ -0,0 +1,43 @@
+###----------------------------------------
+### Homework 2 Assignment: 2+2
+###----------------------------------------
+
+###----------------------------------------
+### The goal of this homework is to assign 
+### variables with numerical values and then
+### add the two variables to get a resulting
+### sum of 4.
+###----------------------------------------
+
+###----------------------------------------
+### Below is how we create the objects "a" 
+### and "b". In order to have RStudio 
+### recognize it is an object, after entering
+### "a <- 2", you must hit "Alt + Enter" to 
+### 'Assign' the object a value. You must do
+### this for each object.
+###----------------------------------------
+
+a <- 2
+b <- 2
+
+###----------------------------------------
+### Next, simply add the two together. The
+### functions in RStudio work exactly as 
+### one would expect. Simply type the two 
+### values you wish to add and hit enter.
+###----------------------------------------
+
+print(a+b)
+
+###----------------------------------------
+### Hitting "Alt + Enter" with the above 
+### command will send the value of "4" to
+### the console. An alternative approach, 
+### is adding the two new sums as a new 
+### object and printing that object. This
+### can be seen below.
+###----------------------------------------
+
+d <- a+b
+print(d)
