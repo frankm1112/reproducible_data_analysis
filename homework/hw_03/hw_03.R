8f6759d074ff8637a674feaaa7a4d74943525a9c
Frank May <fmay@vols.utk.edu>
2020-11-07 12:57:30 -0500

    Comment fixes HW_03

--- homework/hw_03/hw_03.R
+++ homework/hw_03/hw_03.R
@@ -6,52 +6,55 @@
 ### First, input the multiple vectors to be classified.
 ###-----------------------------------------------------
 
-e <- c(TRUE,FALSE,FALSE)
-f <- c(1,2,3)
-g <- c(1.3,2.4,3.5)
-h <- c("a","b", "c")
+boolean_vector <- c(TRUE,FALSE,FALSE)
+numeric_vector <- c(1,2,3)
+numeric_vector_2 <- c(1.3,2.4,3.5)
+character_vector <- c("a","b", "c")
 
 ###-----------------------------------------------------
 ### Next, type the classify commands.
 ###-----------------------------------------------------
 
-class(e)
-class(f)
-class(g)
-class(h)
+class(boolean_vector)
+class(numeric_vector)
+class(numeric_vector_2)
+class(character_vector)
 
 ###------------------------------------------------------
 ### When this code is ran, it lists that the objects
-### e, f, g, and h are logical, numeric, numeric, and
-### character vector classes respectively. Object e is 
-### classified as logical, as the only entries listed 
-### are objects using boolean logic. Objects f and g are
-### numeric as they are only numbers without quotation 
-### marks around them. The addition of quotation marks 
-### would make this a character classed vector. And the 
-### last object, h, is a character vector as it uses
-### neither boolean nor numeric values.
+### 'boolean_vector', 'numeric_vector', 'numeric_vector_2', 
+### and 'character_vector' are logical, numeric, numeric, 
+### and character vector classes respectively. Object 
+### 'boolean_vector' is classified as logical, as the only 
+### entries listed are objects using boolean logic. Objects 
+### 'numeric_vector' and 'numeric_vector' are numeric as 
+### they are only numbers without quotation marks around 
+### them. The addition of quotation marks would make this 
+### a character classed vector. And the last object, 
+### 'character_vector', is a character class vector as it 
+### uses neither boolean nor numeric values.
 ###------------------------------------------------------
 
-i <- c(1,2,"a")
-j <- c(TRUE,FALSE,2)
-class(i)
-class(j)
+numeric_character_vector <- c(1,2,"a")
+boolean_numeric_vector <- c(TRUE,FALSE,2)
+class(numeric_character_vector)
+class(boolean_numeric_vector)
 
 ###------------------------------------------------------
 ### When the classification of each vector is performed
-### you can see that object i is a character vector while
-### object j is a numeric vector. Object i  is listed as 
-### a character vector due to the fact that "a" can only 
-### be stored as a character and not logic or numeric
+### you can see that object 'numeric_character_vector' is 
+### a character vector while object 'boolean_numeric_vector'
+### is a numeric vector. 'numeric_character_vector'  is 
+### listed as a character vector due to the fact that "a" 
+### can only be stored as a character and not logic or numeric
 ### values. 1 and 2, while numeric, can also be stored as
 ### characters, in the case of "1" and "2".That is why this
-### is a character vector. In terms of object j, it is
-### listed as a numeric class as TRUE and FALSE are in 
-### essence just 1 and 0, allowing them to be stored as 
-### numerical values. 2 on the other hand, cannot be stored
-### as Boolean logic, as it only uses 0's and 1's, so it
-### cannot be listed as a logical class.
+### is a character vector. In terms of the object
+### 'boolean_numeric_vector', it is listed as a numeric class 
+### as TRUE and FALSE are in essence just 1 and 0, allowing 
+### them to be stored as numerical values. 2 on the other 
+### hand, cannot be stored as Boolean logic, as it only uses 
+### 0's and 1's, so it cannot be listed as a logical class.
 ###------------------------------------------------------
 
 
